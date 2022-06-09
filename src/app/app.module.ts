5ef6b60c897055170c0c3b39230c4f9d93d17126
LucasKienast <lucas.kienast@hotmail.de>
2022-06-09 23:26:02 +0200

    decorators

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {CoreModule} from "./core/core.module";
+import { UserComponent } from './user/user.component';
+import { TransformDirective } from './transform.directive';
+import { SafePipe } from './pipes/safe/safe.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    TransformDirective,
+    SafePipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule
   ],
+  exports: [],
   providers: [],
   bootstrap: [AppComponent]
 })
